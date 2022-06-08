9ca4c324618cdfa81c5583afa9c2c19d7f8cdb5f
luizfiorentino <lgfiorentino@gmail.com>
2022-06-08 11:35:28 +0200

    feat/ relations established between product and review models

--- models/review.js
+++ models/review.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class review extends Model {
     /**
@@ -10,14 +8,17 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      review.belongsTo(models.product, { foreignKey: "productId" });
     }
   }
-  review.init({
-    userReview: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'review',
-  });
+  review.init(
+    {
+      userReview: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "review",
+    }
+  );
   return review;
 };
