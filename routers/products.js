33bd07629c4c50645bb4a142187ec9c5d5a66553
luizfiorentino <lgfiorentino@gmail.com>
2022-06-14 09:25:53 +0200

    feat/ product details route sending also category infos

--- routers/products.js
+++ routers/products.js
@@ -25,17 +25,21 @@ router.get("/", async (req, res) => {
   }
 });
 
-// GET one product and its category
+// GET one product and its category and reviews
+
 router.get("/:id", async (req, res, next) => {
   try {
     const productId = parseInt(req.params.id);
     const thisProduct = await Product.findByPk(productId, {
-      include: { model: Category },
+      include: { model: Review },
     });
+    const id = thisProduct.categoryId;
+    const category = await Category.findByPk(id);
+
     if (!thisProduct) {
       res.status(404).send("This id does not match any of the products'");
     } else {
-      res.send(thisProduct);
+      res.send({ thisProduct, category });
     }
   } catch (e) {
     next(e);
@@ -55,24 +59,24 @@ router.get("/reviews/all", async (req, res, next) => {
 });
 
 // GET all reviews for a product
-router.get("/reviews/:prodId", async (req, res, next) => {
-  try {
-    const prodId = parseInt(req.params.prodId);
-    const thisProduct = await Product.findByPk(parseInt(prodId));
-    if (!thisProduct) {
-      res.status(404).send("Product not found");
-    } else {
-      const reviews = await Review.findAll({
-        where: { productId: prodId },
-      });
-      //  where: { userId: req.user.id },
-      res.status(200).send(reviews);
-    }
-  } catch (e) {
-    console.log(e);
-    next(e);
-  }
-});
+// router.get("/reviews/:prodId", async (req, res, next) => {
+//   try {
+//     const prodId = parseInt(req.params.prodId);
+//     const thisProduct = await Product.findByPk(parseInt(prodId));
+//     if (!thisProduct) {
+//       res.status(404).send("Product not found");
+//     } else {
+//       const reviews = await Review.findAll({
+//         where: { productId: prodId },
+//       });
+//       //  where: { userId: req.user.id },
+//       res.status(200).send(reviews);
+//     }
+//   } catch (e) {
+//     console.log(e);
+//     next(e);
+//   }
+// });
 
 // router.get("/reviews/:id", async (req, res, next) => {
 //   try {
